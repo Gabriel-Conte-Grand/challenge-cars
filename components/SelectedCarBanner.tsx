59479940b769aa68978145007419ba905a2bd867
Gabriel Conte-Grand <gabrielcontegrand10@gmail.com>
2025-06-04 11:22:42 -0300

    dynamic banner

--- components/SelectedCarBanner.tsx
+++ components/SelectedCarBanner.tsx
@@ -34,11 +34,28 @@ export const SelectedCarBanner = ({
 
   const imageAddress = image?.split("/").at(-1);
 
+  const numberOfDays = 3;
+  const formattedPriceCOP = (
+    carOfInterest.rates?.F2?.pricing.COP.total_charge.total
+      .estimated_total_amount * numberOfDays
+  ).toLocaleString("es-CO", {
+    style: "currency",
+    currency: "COP",
+  });
+
+  const formattedPriceUSD = (
+    carOfInterest.rates?.F2?.pricing.USD.total_charge.total
+      .estimated_total_amount * numberOfDays
+  ).toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
   return (
     <div className="fixed bottom-0 w-screen py-4 bg-white  border-t border-gray-200 z-50 shadow-[0_-4px_4px_-1px_rgba(0,0,0,0.1),0_-2px_2px_-2px_rgba(0,0,0,0.1)]">
       <div className="max-w-7xl mx-auto gap-12 flex items-center ">
         <Image src={retailerLogo} alt="logo" width={48} height={15} />
-        <div className="flex flex-col gap-[6px] py-2 w-96">
+        <div className="flex flex-col gap-[6px] py-2 w-80">
           <h4 className="text-[#242B35] text-xl font-bold ">
             Inclusive Light - H8
           </h4>
@@ -52,36 +69,30 @@ export const SelectedCarBanner = ({
           width={132}
           height={62}
         />
-        <div className="flex gap-8 py-2">
-          <div className="flex flex-col gap-[6px]  items-end">
+        <div className="flex justify-between gap-14">
+          <div className="flex flex-col gap-[6px]   items-end">
             <h4 className="text-[#242B35] text-xl font-bold ">
-              COP{" "}
-              {
-                carOfInterest?.rates?.F2?.pricing.COP.total_charge.total
-                  .estimated_total_amount
-              }
+              COP {formattedPriceCOP}
             </h4>
             <p className="text-[#8292AA] text-sm font-medium">
-              USD{" "}
-              {
-                carOfInterest.rates?.F2?.pricing.USD.total_charge.total
-                  .estimated_total_amount
-              }
+              USD {formattedPriceUSD}
             </p>
           </div>
-          <button
-            onClick={() => addCarToSelected(carOfInterest)}
-            className="bg-[#3179BD] cursor-pointer hover:opacity-80 py-2 px-10 font-bold text-white rounded-lg"
-          >
-            Continuar
-          </button>
-          <button
-            onClick={deleteCarFromSelected}
-            className="bg-[#E91248] flex gap-5 items-center cursor-pointer hover:opacity-80 py-2 px-10 font-bold  text-white rounded-lg"
-          >
-            <Image src={deleteLogo} alt="logo" width={12} height={12} />{" "}
-            <span>Eliminar</span>
-          </button>
+          <div className="flex-1 gap-8 flex">
+            <button
+              onClick={() => addCarToSelected(carOfInterest)}
+              className="bg-[#3179BD] cursor-pointer hover:opacity-80 py-2 px-10 font-bold text-white rounded-lg"
+            >
+              Continuar
+            </button>
+            <button
+              onClick={deleteCarFromSelected}
+              className="bg-[#E91248] flex gap-5 items-center cursor-pointer hover:opacity-80 py-2 px-10 font-bold  text-white rounded-lg"
+            >
+              <Image src={deleteLogo} alt="logo" width={12} height={12} />{" "}
+              <span>Eliminar</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
